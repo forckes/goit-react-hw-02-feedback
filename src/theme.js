c85c129d5035be9b09352671e995d73a3a601a17
forckes <forckes@proton.me>
2023-04-03 21:28:45 +0300

    Some theming

---
+++ src/theme.js
@@ -0,0 +1,38 @@
+export const theme = {
+  colors: {
+    black: '#000',
+    grey: '#74747474',
+    text: '#2a2a2a',
+    background: '#fff',
+    active: '#74747447',
+    hover: '#74747423',
+  },
+  space: [0, 2, 4, 8, 16, 32, 64, 128, 256],
+  fonts: {
+    body: 'system-ui, sans-serif',
+    heading: 'system-ui, sans-serif',
+    monospace: 'Menlo, monospace',
+  },
+  fontSizes: {
+    m: '20px',
+    l: '24px',
+    xl: '40px',
+  },
+  fontWeights: {
+    normal: 400,
+    bold: 600,
+  },
+  lineHeights: {
+    body: 1.5,
+    heading: 1.125,
+  },
+  borders: {
+    none: 'none',
+    normal: '1px solid',
+  },
+  radii: {
+    none: '0',
+    normal: '4px',
+    round: '50%',
+  },
+};
